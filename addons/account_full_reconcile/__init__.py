496b1550c03d26af5b84b87fbf7b60c3f1f071a2
Ronald Portier <ronald@therp.nl>
2016-12-16 12:20:54 +0100

    [WIP] Compute reconciled in account_move_line and more efficient reading.

--- addons/account_full_reconcile/__init__.py
+++ addons/account_full_reconcile/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-# © 2016 Therp BV.
 from openerp import SUPERUSER_ID
-from openerp.tools.float_utils import float_round
+from openerp.tools.float_utils import float_round, float_is_zero
 
 import models
 
@@ -25,15 +24,17 @@ def _migrate_full_reconcile(cr, registry):
         def __init__(self, db_record):
             """Initialize attributes from db record."""
             self.id = db_record[0]
-            self.reconcile_id = db_record[1]
-            self.full_reconcile = db_record[2]
+            self.combined_reconcile_id = db_record[1]
+            self.reconcile_id = db_record[2]
             self.reconcile_ref = db_record[3]
             self.debit = db_record[4]
             self.credit = db_record[5]
-            self.currency_id = db_record[6]
-            self.date = db_record[7]
-            self.amount_residual = db_record[8]
-            self.rounding = db_record[9]
+            self.date = db_record[6]
+            self.amount_residual = db_record[7]  # Start off with full amount
+            self.line_currency_id = db_record[8]
+            self.line_currency_rounding = db_record[9]
+            self.company_currency_id = db_record[10]
+            self.company_currency_rounding = db_record[11]
             self.amount_residual_currency = 0.0  # Not in 8.0
 
     def reconcile_records(cr, debit_record, credit_record, full_reconcile_id):
@@ -97,12 +98,32 @@ def _migrate_full_reconcile(cr, registry):
         """Update move lines."""
         # TODO: compute amount_currency
         for line in move_lines:
+            # Compute reconciled similar to what happens in model, but using
+            # data retrieved using SQL:
+            reconciled = False
+            digits_rounding_precision = line.company_currency_rounding
+            if float_is_zero(
+                    line.amount_residual,
+                    precision_rounding=digits_rounding_precision):
+                if line.line_currency_id and line.amount_residual_currency:
+                    # if there is an amount in another currency, it must
+                    # be zero as well:
+                    currency_zero = float_is_zero(
+                        line.amount_residual_currency,
+                        precision_rounding=line.line_currency_id_rounding
+                    )
+                    if currency_zero:
+                        reconciled = True
+                else:
+                    # no currency involved:
+                    reconciled = True
             cr.execute(
                 """
                 UPDATE account_move_line SET
                     full_reconcile_id = %s,
                     amount_residual = %s,
                     amount_residual_currency = %s,
+                    reconciled = %s,
                     write_date = CURRENT_TIMESTAMP,
                     write_uid = %s
                 WHERE id = %s
@@ -117,6 +138,7 @@ def _migrate_full_reconcile(cr, registry):
                         line.amount_residual_currency,
                         precision_rounding=line.rounding
                     ),
+                    reconciled,
                     SUPERUSER_ID,
                     line.id
                 )
@@ -129,7 +151,7 @@ def _migrate_full_reconcile(cr, registry):
             return
         record = debit_lines[0]
         full_reconcile_id = (
-            record.full_reconcile and
+            record.reconcile_id and
             create_full_reconcile(cr, record.reconcile_ref) or
             False
         )
@@ -162,24 +184,42 @@ def _migrate_full_reconcile(cr, registry):
         update_account_move_line(cr, debit_lines, full_reconcile_id)
         update_account_move_line(cr, credit_lines, full_reconcile_id)
 
+    # Define generator to loop over potentially huge amount of records in
+    # cursor
+    def result_iter(cursor, arraysize=1024):
+        'An iterator that uses fetchmany to keep memory usage down'
+        while True:
+            results = cursor.fetchmany(arraysize)
+            if not results:
+                break
+            for result in results:
+                yield result
+
+    # We need information on possibly two currencies. One for the company
+    # one for the alternative currency defined in the line. For clarity
+    # we will prefix the first with company_currency_ and the other with
+    # line_currency_
     cr.execute(
         """
         SELECT
             aml.id AS id,
             COALESCE(aml.reconcile_id, aml.reconcile_partial_id)
-                AS reconcile_id,
+                AS combined_reconcile_id,
             COALESCE(aml.reconcile_id, 0)
-                AS full_reconcile,
+                AS reconcile_id,
             aml.reconcile_ref,
             aml.debit,
             aml.credit,
-            aml.currency_id,
             aml.date AS date,
             aml.debit - aml.credit AS amount_residual,
-            cur.rounding AS rounding
+            aml.currency_id as line_currency_id,
+            line_cur.rounding AS line_currency_rounding
+            com.currency_id as company_currency_id,
+            company_cur.rounding AS company_currency_rounding
         FROM account_move_line aml
         JOIN res_company com on aml.company_id = com.id
-        JOIN res_currency cur on com.currency_id = cur.id
+        JOIN res_currency line_cur on aml.currency_id = line_cur.id
+        JOIN res_currency company_cur on com.currency_id = company_cur.id
         WHERE aml.reconcile_id IS NOT NULL
            OR aml.reconcile_partial_id IS NOT NULL
         ORDER BY
@@ -189,13 +229,13 @@ def _migrate_full_reconcile(cr, registry):
     current_id = False
     debit_lines = []
     credit_lines = []
-    for db_record in cr.fetchall():
+    for db_record in result_iter(cr):
         record = LineRecord(db_record)
-        if current_id and current_id != record.reconcile_id:
+        if current_id and current_id != record.combined_reconcile_id:
             handle_complete_reconciliation(cr, debit_lines, credit_lines)
             debit_lines = []  # Reset
             credit_lines = []  # Reset
-        current_id = record.reconcile_id
+        current_id = record.combined_reconcile_id
         if record.credit:
             record.amount_residual = record.credit
             credit_lines.append(record)
